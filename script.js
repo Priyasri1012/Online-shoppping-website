5fad2b47b2f33c565249e35b3ada934ecc2710f9
Priya Srinivasan <107395414+Priyasri1012@users.noreply.github.com>
2022-07-13 12:10:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var Menuitems = document.getElementById("menuitems");
+Menuitems.style.maxHeight ="0px";
+function menutoggle(){
+    if(Menuitems.style.maxHeight =="0px")
+    {
+        Menuitems.style.maxHeight =="200px";
+    }
+    else
+    {
+        Menuitems.style.maxHeight =="0px";
+    }
+}
